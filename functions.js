4a5b393e17c85435d551be96e010b95272873c25
Cristian Ramirez <ccrhdbj@outlook.com>
2024-09-06 07:42:01 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ export const data = {
     { shortName: "Gratinada", name: "Gratinada", price: 6300 },
     { shortName: "Gorda", name: "Gorda", price: 1300 },
     { shortName: "Gorda Q", name: "Gorda Queso", price: 3500 },
-    { shortName: "Gorda L", name: "Gorda Loncha", price: 2200},
+    { shortName: "Gorda L", name: "Gorda Loncha", price: 2900},
     { shortName: "Sin sal", name: "Sin sal", price: 600 },
     { shortName: "Sin sal Q", name: "Sin sal Queso", price: 3000 },
     { shortName: "Sin Sal L", name: "Sin Sal Loncha", price: 2200},
